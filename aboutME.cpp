f7b8604ddbf7575169ba3ee1107b36f8d4cbb316
kiyo-domon <reneredtrejo@yahoo.com>
2015-05-20 12:47:34 -0500

    first commit to the repositaoty which is frist small program

---
+++ aboutME.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//fucntino porrotye
+
+void aboutME();
+
+int main()
+{
+	// this is the very first
+
+	aboutME();
+	
+	return 0;
+}
+
+void aboutME()
+{
+
+	cout << "\nHello my name is Rene Trejo";
+	cout << "I started college and have started learneing programming" << endl;
+
+	cout << "\n";
+
+	cout << "Im currenty learning c++ but have done small work in pythong\n";
+
+	cout << "I hope to someday be a software developer ";
+
+
+
+}
